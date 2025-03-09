680dd5a4592cefd051d85b142360aa6592c30486
Shaashvat Mittal <shaashvatmittal2004@gmail.com>
2025-03-09 15:59:53 -0700

    Initial commit

---
+++ Zesto/Zesto/ZestoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZestoApp.swift
+//  Zesto
+//
+//  Created by Shaashvat Mittal on 3/9/25.
+//
+
+import SwiftUI
+
+@main
+struct ZestoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
