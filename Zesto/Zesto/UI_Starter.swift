17126dd5fa664c0a5d1dfea453c485ff2545a15f
saisrivathsan9 <58337977+saisrivathsan9@users.noreply.github.com>
2025-03-14 19:20:39 -0700

    Updated UI template

--- Zesto/Zesto/UI_Starter.swift
+++ Zesto/Zesto/UI_Starter.swift
@@ -10,8 +10,99 @@ import SwiftUI
 struct UI_Starter: View {
     var body: some View {
         VStack{
-            Text("Hello, from template!")
+            ZStack{
+                Rectangle()
+                    .fill(Color.green)
+                    .frame(width: .infinity, height: 100)
+                
+                Image("LOGO")
+                    .resizable()
+                    .frame(width: 150, height: 60)
+                    .offset(x: -110, y: 20)
+                
+                
+                Button {
+                    
+                } label: {
+                    Image("MenuBar")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                }
+                .offset(x: 170, y: 30)
+
+                
+            }
+            
+            
+            Spacer()
+            
+            VStack{
+                Text("Hello, from template!")
+            }
+            
+            
+            Spacer()
+            
+            ZStack{
+                Rectangle()
+                    .fill(Color.green)
+                    .frame(width: .infinity, height: 75)
+                
+                Circle()
+                    .fill(Color.white)
+                    .shadow(radius: 10)
+                    .frame(width: 50, height: 50)
+                    .offset(y: -40)
+                
+                Button {
+                    
+                } label: {
+                    Image("Home")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                }
+                .offset(y: -40)
+                
+                Button {
+                    
+                } label: {
+                    Image("Scan")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                }
+                .offset(x: -130)
+                
+                Button {
+                    
+                } label: {
+                    Image("Fridge")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                }
+                .offset(x: -40)
+                
+                Button {
+                    
+                } label: {
+                    Image("Chat")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                }
+                .offset(x: 40)
+                
+                Button {
+                    
+                } label: {
+                    Image("Community")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                }
+                .offset(x: 130)
+            }
+            
         }
+        .ignoresSafeArea(.all)
+        
         
         
     }
