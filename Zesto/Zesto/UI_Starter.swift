8a2e996fe3545ed3e9fae7e2ed78aa790a7b644f
saisrivathsan9 <58337977+saisrivathsan9@users.noreply.github.com>
2025-03-14 18:20:48 -0700

    Created UI starter template file

---
+++ Zesto/Zesto/UI_Starter.swift
@@ -0,0 +1,23 @@
+//
+//  UI_Template.swift
+//  Zesto
+//
+//  Created by Saisrivathsan Manikandan on 3/14/25.
+//
+
+import SwiftUI
+
+struct UI_Starter: View {
+    var body: some View {
+        VStack{
+            Text("Hello, from template!")
+        }
+        
+        
+    }
+}
+
+#Preview {
+    UI_Starter()
+}
+
